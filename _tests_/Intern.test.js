16b2e26be2839e4231ed568155111e05ebdd63b3
Charlie Parrott <cparrott5700@gmail.com>
2021-08-31 16:33:38 -0400

    Adding intern test

---
+++ _tests_/Intern.test.js
@@ -0,0 +1,33 @@
+const Employee = require("../lib/Employee");
+const Intern = require ("../lib/Intern");
+
+describe("Intern Class", () => {
+    it ("Initiates a new intern selection", () => {
+        const intern = new Intern();
+        expect(new Intern(intern)).toBe("object");
+    });
+
+    describe("const school", () => {
+        it("Can enter school through the constructor argument", () => {
+            const testSchool = "College";
+            const intern = new Intern(testSchool);
+            expect(intern.school).toBe(testSchool);
+        });
+    });
+
+    describe("getSchool", () => {
+        it("Can get school through getSchool()", () => {
+            const testSchool = "College";
+            const intern = new School(testSchool);
+            expect(intern.getSchool()).toBe(testSchool);
+        });
+    });
+
+    describe("getRole", () => {
+        it("Returns Intern through getRole()", () => {
+            const testRole = "Intern";
+            const intern = new Intern("Charles", 81, "email@testRole.com", testSchool);
+            expect(intern.getRole()).toBe(testRole);
+        });
+    });
+});
